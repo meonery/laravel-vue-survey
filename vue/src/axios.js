bda5ac816c8a049cf52525b5d8478606169daf42
meonery <mayrieleerlgen.nery@gmail.com>
2022-12-16 00:22:35 +0800

    added notfound file

--- vue/src/axios.js
+++ vue/src/axios.js
@@ -1,13 +1,28 @@
 import axios from "axios";
 import store from "./store";
+import router from "./router";
 
 const axiosClient = axios.create({
-    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
 })
 
+
 axiosClient.interceptors.request.use(config => {
-    config.headers.Authorization = `Bearer ${store.state.user.token}`;
-    return config;
+  config.headers.Authorization = `Bearer ${store.state.user.token}`
+  // config.headers['Content-Type'] = `application/json`;
+  return config;
+})
+
+axiosClient.interceptors.response.use(response => {
+  return Promise.resolve(response);
+}, error => {
+  if (error.response.status === 401) {
+    sessionStorage.removeItem('TOKEN')
+    router.push({name: 'Login'})
+  } else if (error.response.status === 404) {
+    router.push({name: 'NotFound'})
+  }
+  return Promise.reject(error);
 })
 
 export default axiosClient;
